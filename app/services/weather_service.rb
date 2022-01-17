eb37d925635ec2316f6e09ecca4817c4ebbe62d1
Micha Bernhard <mib70@pitt.edu>
2022-01-17 10:44:41 -0700

    Change params passed in the service

---
+++ app/services/weather_service.rb
@@ -0,0 +1,18 @@
+class WeatherService
+  class << self
+    def get_weather(lat, lon)
+      response = conn.post("/data/2.5/onecall?lat=#{lat}&lon=#{lon}&appid=#{ENV['appid']}&units=imperial&exclude=minutely")
+      parse_data(response)
+    end
+
+    private
+
+    def conn
+      Faraday.new("https://api.openweathermap.org")
+    end
+
+    def parse_data(response)
+      JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+end
--- spec/services/weather_service_spec.rb
+++ spec/services/weather_service_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Weather service spec' do
   describe 'OpenWeather api call' do
     it 'returns weather information' do
-      params = {
-        lat: '39.738453',
-        lon: '-104.984853'
-      }
+      lat = '39.738453'
+      lon = '-104.984853'
 
-      weather = WeatherService.get_weather(params)
+      weather = WeatherService.get_weather(lat, lon)
 
       expect(weather).to be_a Hash
+      expect(weather[:hourly]).to_not eq(nil)
+      expect(weather[:daily]).to_not eq(nil)
+      expect(weather[:minutely]).to eq(nil)
     end
   end
 end
