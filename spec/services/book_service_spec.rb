6a55a09ffaf8fe89bb1173dceea94701711cf4fc
Micha Bernhard <mib70@pitt.edu>
2022-01-18 09:23:54 -0700

    Change 5 int into a string

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Book Service spec' do
   describe 'Openlibrary api call' do
     it 'returns the result of an Openlibrary API call' do
       location = 'denver,co'
-      quantity = 5
+      quantity = '5'
 
       books = BookService.get_books(location, quantity)
 
