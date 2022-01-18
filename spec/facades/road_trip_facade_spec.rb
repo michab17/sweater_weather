d5680545b2bf12cfe15720a90832485fc079ad80
Micha Bernhard <mib70@pitt.edu>
2022-01-18 16:41:52 -0700

    Test happy and sad paths for the road trip facade

---
+++ spec/facades/road_trip_facade_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Road Trip Facade spec' do
+  describe 'creates Road Trip poros' do
+    it 'returns and instance of a Road Trip with information if it is possible' do
+      from = 'Denver, CO'
+      to = 'Estes Park, CO'
+
+      road_trip = RoadTripFacade.get_directions(from, to)
+
+      expect(road_trip).to be_a RoadTrip
+      expect(road_trip.start_city).to eq 'Denver, CO'
+      expect(road_trip.end_city).to eq 'Estes Park, CO'
+      expect(road_trip.travel_time).to be_a String
+      expect(road_trip.weather_at_eta).to be_a Hash
+      expect(road_trip.weather_at_eta.keys).to eq [:temperature, :conditions]
+    end
+
+    it 'returns and instance of a Road Trip with information if it is possible' do
+      from = 'Denver, CO'
+      to = 'London, UK'
+
+      road_trip = RoadTripFacade.get_directions(from, to)
+
+      expect(road_trip).to be_a RoadTrip
+      expect(road_trip.start_city).to eq 'Denver, CO'
+      expect(road_trip.end_city).to eq 'London, UK'
+      expect(road_trip.travel_time).to be_a String
+      expect(road_trip.travel_time).to eq 'impossible route'
+      expect(road_trip.weather_at_eta).to be_a Hash
+      expect(road_trip.weather_at_eta).to eq {temperature: "", conditions: ""}
+    end
+  end
+end
