1880a2db33f4a309a38adc10f2b4f63baf93431d
Micha Bernhard <mib70@pitt.edu>
2022-01-18 10:49:26 -0700

    Add edge case coverage of quantity being < 0

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-    render json: BookSerializer.new(BookFacade.get_books(params[:location], params[:quantity]))
+    if params[:quantity].to_i > 0
+      render json: BookSerializer.new(BookFacade.get_books(params[:location], params[:quantity]))
+    else
+      render json: {error: 'Please enter a quantity greater than zero', status: 422}, status: 422
+    end
   end
 end
