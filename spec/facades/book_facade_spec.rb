e9830a78d09c0fe5d5afb39249be9eb572885365
Micha Bernhard <mib70@pitt.edu>
2022-01-18 09:51:28 -0700

    Adjust tests for poro data structure

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Books facade spec' do
-  it 'uses the data from the service to create a poro' do
+  it 'uses the data from the service to create a poro', :vcr do
     location = 'denver,co'
     quantity = '5'
 
@@ -9,7 +9,11 @@ RSpec.describe 'Books facade spec' do
 
     expect(books).to be_a(Book)
     expect(books.id).to eq 'null'
+    expect(books.type).to eq 'books'
     expect(books.total_books_found).to eq 41221
     expect(books.books.count).to eq 5
+    expect(books.books[0].keys).to eq [:isbn, :title, :publisher]
+    expect(books.forecast).to be_a Hash
+    expect(books.forecast[:forecast].keys).to eq [:summary, :temperature]
   end
 end
