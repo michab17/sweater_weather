8840b5c8c4b1a53d890912584949bbf25a3eacee
Micha Bernhard <mib70@pitt.edu>
2022-01-17 17:13:14 -0700

    change structure of poro data

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -8,8 +8,48 @@ class Forecast
   def initialize(data)
     @id = 'null'
     @type = 'forecast'
-    @current_weather = data[:current]
-    @daily_weather = data[:daily]
-    @hourly_weather = data[:hourly]
+    @current_weather = current(data[:current])
+    @daily_weather = daily(data[:daily])
+    @hourly_weather = hourly(data[:hourly])
+  end
+
+  def current(data)
+    { 
+      datetime: Time.at(data[:dt]).to_datetime,
+      sunrise: Time.at(data[:sunrise]).to_datetime,
+      sunset: Time.at(data[:sunset]).to_datetime,
+      temperature: data[:temp],
+      feels_like: data[:feels_like],
+      humidity: data[:humidity],
+      uvi: data[:uvi],
+      visibility: data[:visibility],
+      conditions: data[:weather][0][:description],
+      icon: data[:weather][0][:icon]
+     }
+  end
+
+  def daily(data)
+    data[0..4].map do |day|
+      {
+        date: Time.at(day[:dt]).to_date,
+        sunrise: Time.at(day[:sunrise]).to_datetime,
+        sunset: Time.at(day[:sunset]).to_datetime,
+        max_temp: day[:temp][:max],
+        min_temp: day[:temp][:min],
+        conditions: day[:weather][0][:description],
+        icon: day[:weather][0][:icon]
+        }
+    end
+  end
+
+  def hourly(data)
+    data[0..7].map do |hour|
+      {
+        time: Time.at(hour[:dt]).to_time,
+        temperature: hour[:temp],
+        conditions: hour[:weather][0][:description],
+        icon: hour[:weather][0][:icon]
+        }
+    end
   end
 end
