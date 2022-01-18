f436d0eb8182989c10ca64d2afab7ad4fde6a5fd
Micha Bernhard <mib70@pitt.edu>
2022-01-17 21:38:30 -0700

    Test the unsplash api service

---
+++ spec/services/image_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Image service spec' do
+  describe 'calls the unsplash api' do
+    it 'returns image information based on a search query' do
+      query = 'denver,co'
+
+      image_data = ImageService.get_image(query)
+
+      expect(image_data).to be_a Hash
+      expect(image_data[:results]).to be_an Array
+      expect(image_data[:results][0]).to be_an Hash
+      expect(image_data[:results][0][:urls]).to be_an Hash
+      expect(image_data[:results][0][:urls][:raw]).to be_a String
+    end
+  end
+end
