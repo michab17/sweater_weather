a734d762d52ff4d782fb8bfae25d26667e8b4fbf
Micha Bernhard <mib70@pitt.edu>
2022-01-17 17:15:23 -0700

    render the forecast in the forecast index method

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    render json: ForecastSerializer.new(ForecastFacade.get_weather())
+    render json: ForecastSerializer.new(ForecastFacade.get_weather(LocationFacade.get_location(params[:location]).latitude, LocationFacade.get_location(params[:location]).longitude))
   end
 end
