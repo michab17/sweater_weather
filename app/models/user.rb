5f40fdb2ba566e517a20f379a981d3343e707448
Micha Bernhard <mib70@pitt.edu>
2022-01-18 13:41:45 -0700

    Add api key generation method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
+require 'securerandom'
+
 class User < ApplicationRecord
   validates_presence_of :email
   validates_uniqueness_of :email
   validates_presence_of :password_digest
 
   has_secure_password
+
+  def self.create_api_key
+    random_string = SecureRandom.hex
+  end
 end
