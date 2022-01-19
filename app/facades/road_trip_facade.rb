62a9287969a51392fe9432602e99387ecde060a1
Micha Bernhard <mib70@pitt.edu>
2022-01-18 18:39:07 -0700

    Pass additional params to poro

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -3,7 +3,7 @@ class RoadTripFacade
     def get_directions(from, to)
       direction_data = RoadTripService.get_directions(from, to)
 
-      RoadTrip.new(direction_data)
+      RoadTrip.new(direction_data, from, to)
     end
   end
 end
