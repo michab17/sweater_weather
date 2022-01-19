a2a1a2ae0a6dd372c22307204df20a1562857f36
Micha Bernhard <mib70@pitt.edu>
2022-01-18 20:28:40 -0700

    Make if statement more clear

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::RoadTripController < ApplicationController
   def index
     user = User.find_by(api_key: params[:api_key])
-    if user
+    if user != nil
       render json: RoadTripSerializer.new(RoadTripFacade.get_directions(params[:origin], params[:destination]))
     else
       render json: {error: 'API key was not passed or is invalid', status: 401}, status: 401
