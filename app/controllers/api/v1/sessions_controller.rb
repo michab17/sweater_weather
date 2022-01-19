3b906730335a3442a89d3e54f6cc55491f49aa92
Micha Bernhard <mib70@pitt.edu>
2022-01-18 19:01:51 -0700

    pass user id into sessions[:user_id]

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       render json: UsersSerializer.new(user), status: 200
     else
       render json: {error: 'Email or password is incorrect', status: 400}, status: 400
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::UsersController < ApplicationController
       if params[:password] == params[:password_confirmation]
         user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: User.create_api_key)
         if user.save
+          session[:user_id] = user.id
           render json: UsersSerializer.new(user), status: 201
         else
           render json: {error: 'All fields must be filled in', status: 400}, status: 400
