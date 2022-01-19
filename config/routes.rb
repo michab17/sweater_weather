e084da7fffa17271cf801d76c0549d8cb1fe670d
Micha Bernhard <mib70@pitt.edu>
2022-01-18 19:05:27 -0700

    Add road trip api call to routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :backgrounds, only: [:index]
       post '/users', to: 'users#create'
       post '/sessions', to: 'sessions#create'
+      post '/road_trip', to: 'road_trip#index'
     end
   end
 end
