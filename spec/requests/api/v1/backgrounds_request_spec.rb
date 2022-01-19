7eeedfc3d3109a7bfeb6b6047107d424c5b5ce91
Micha Bernhard <mib70@pitt.edu>
2022-01-18 21:34:35 -0700

    Update tests

--- spec/requests/api/v1/backgrounds_request_spec.rb
+++ spec/requests/api/v1/backgrounds_request_spec.rb
@@ -14,7 +14,13 @@ RSpec.describe 'Image request Spec' do
       expect(image[:data][:type]).to eq 'image'
       expect(image[:data][:attributes]).to be_a Hash
       expect(image[:data][:attributes].keys).to eq [:description, :image_url, :credit_information]
+      expect(image[:data][:attributes][:description]).to be_a String
+      expect(image[:data][:attributes][:image_url]).to be_a String
+      expect(image[:data][:attributes][:credit_information]).to be_a Hash
       expect(image[:data][:attributes][:credit_information].keys).to eq [:source, :photographer, :account_page]
+      expect(image[:data][:attributes][:credit_information][:source]).to be_a String
+      expect(image[:data][:attributes][:credit_information][:photographer]).to be_a String
+      expect(image[:data][:attributes][:credit_information][:account_page]).to be_a String
     end
   end
 end
--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -9,8 +9,24 @@ RSpec.describe 'Forecast show spec' do
 
       expect(response).to be_successful
       expect(forecast[:data][:attributes][:current_weather].keys).to eq [:datetime, :sunrise, :sunset, :temperature, :feels_like, :humidity, :uvi, :visibility, :conditions, :icon]
+      expect(forecast[:data][:attributes][:current_weather][:temperature]).to be_a Float
+      expect(forecast[:data][:attributes][:current_weather][:feels_like]).to be_a Float
+      expect(forecast[:data][:attributes][:current_weather][:humidity]).to be_an Integer
+      expect(forecast[:data][:attributes][:current_weather][:uvi]).to be_an Integer
+      expect(forecast[:data][:attributes][:current_weather][:visibility]).to be_an Integer
+      expect(forecast[:data][:attributes][:current_weather][:conditions]).to be_a String
+      expect(forecast[:data][:attributes][:current_weather][:icon]).to be_a String
       expect(forecast[:data][:attributes][:daily_weather][0].keys).to eq [:date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon]
+      expect(forecast[:data][:attributes][:daily_weather][0][:date]).to be_a String
+      expect(forecast[:data][:attributes][:daily_weather][0][:max_temp]).to be_a Float
+      expect(forecast[:data][:attributes][:daily_weather][0][:min_temp]).to be_a Float
+      expect(forecast[:data][:attributes][:daily_weather][0][:conditions]).to be_a String
+      expect(forecast[:data][:attributes][:daily_weather][0][:icon]).to be_a String
       expect(forecast[:data][:attributes][:hourly_weather][0].keys).to eq [:time, :temperature, :conditions, :icon]
+      expect(forecast[:data][:attributes][:hourly_weather][0][:time]).to be_a String
+      expect(forecast[:data][:attributes][:hourly_weather][0][:temperature]).to be_a Float
+      expect(forecast[:data][:attributes][:hourly_weather][0][:conditions]).to be_a String
+      expect(forecast[:data][:attributes][:hourly_weather][0][:icon]).to be_a String
     end
   end
 end
